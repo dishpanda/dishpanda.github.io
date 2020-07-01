3c630f7139df39288fd33a877b525a8ab67f54e6
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2020-06-30 23:24:18 -0700

    increase readability on smaller screens and move inline styling to external CSS

--- src/App.js
+++ src/App.js
@@ -2,19 +2,9 @@ import React from "react";
 import "./App.css";
 import "98.css";
 
-import { Link } from "react-router-dom";
-
 function App() {
   return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#39424e",
-      }}
-    >
+    <div className="site-container">
       <div className="window-container">
         <div className="window">
           <div className="title-bar">
@@ -22,14 +12,15 @@ function App() {
             <div className="title-bar-controls">
               <button aria-label="Minimize" />
               <button aria-label="Maximize" />
-              <button aria-label="Close"></button>
+              <button aria-label="Close" />
             </div>
           </div>
 
           <div className="window-body">
             <h2>Hi there! I'm Rahul.</h2>
             <p>
-              I'll be a freshman at Cal Poly SLO this fall, majoring in Computer Science.
+              I'll be a freshman at Cal Poly SLO this fall, majoring in Computer
+              Science.
             </p>
             <p>
               I’m fascinated by mathematics, computer science, robotics, and the
@@ -38,10 +29,10 @@ function App() {
             <p>
               When I’m not using a computer, I like to write poetry, learn
               foreign languages, shoot photos on film, and play the Indian
-              classical flute–the bansuri. At a party, you’ll find me talking
-              to anyone who’ll lend an ear. Don’t hesitate to get in touch :)
+              classical flute–the bansuri. At a party, you’ll find me talking to
+              anyone who’ll lend an ear. Don’t hesitate to get in touch :)
             </p>
-            <div className="field-row" style={{ justifyContent: "center" }}>
+            <div className="field-row footer" style={{ justifyContent: "center" }}>
               <a href="https://github.com/dishpanda">
                 <button>GitHub</button>
               </a>
