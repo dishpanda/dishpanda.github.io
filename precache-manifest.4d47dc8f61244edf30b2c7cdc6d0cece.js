64b7b5a55c6e673721bc2f156c335bbfe69259c6
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2020-06-30 22:55:52 -0700

    Updates

---
+++ precache-manifest.4d47dc8f61244edf30b2c7cdc6d0cece.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f01e715ccfeeb46550c75dfa1ee268a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5e0e814036248c2a727",
+    "url": "/static/css/2.3979b7dc.chunk.css"
+  },
+  {
+    "revision": "4d3f32bc1e05fd280cc8",
+    "url": "/static/css/main.2d743984.chunk.css"
+  },
+  {
+    "revision": "a5e0e814036248c2a727",
+    "url": "/static/js/2.71124b24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.71124b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d3f32bc1e05fd280cc8",
+    "url": "/static/js/main.432b6d67.chunk.js"
+  },
+  {
+    "revision": "77ca215a6c92fb2d61f5",
+    "url": "/static/js/runtime-main.86e4cacc.js"
+  },
+  {
+    "revision": "1d4d404a3bbe4b7007f89e792233aa9c",
+    "url": "/static/media/ms_sans_serif.1d4d404a.woff2"
+  },
+  {
+    "revision": "7bbbd32e0f9977dd70a4d1287927d5da",
+    "url": "/static/media/ms_sans_serif.7bbbd32e.woff"
+  },
+  {
+    "revision": "6eb2c63fbead0014ef0c4c537bfe328a",
+    "url": "/static/media/ms_sans_serif_bold.6eb2c63f.woff"
+  },
+  {
+    "revision": "cb1c7473de82bb53662aaa2d7640e53e",
+    "url": "/static/media/ms_sans_serif_bold.cb1c7473.woff2"
+  }
+]);
