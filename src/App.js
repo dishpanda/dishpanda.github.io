f5f79eae8dae0420b38f0da29c73f660b52bb8c6
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2020-07-19 11:49:51 -0700

    finished product! for now.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
 import React from "react";
 import "./App.css";
-import "98.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="site-container">
-      <div className="window-container">
-        <div className="window">
-          <div className="title-bar">
-            <div className="title-bar-text">Rahul Deshpande's Website</div>
-            <div className="title-bar-controls">
-              <button aria-label="Minimize" />
-              <button aria-label="Maximize" />
-              <button aria-label="Close" />
+    <div className="page">
+      <div className="flex-container">
+        <div className="left">
+          <Container>
+            <div className="left-body">
+              <h3 className="text-uppercase">Hello, I'm</h3>
+              <div class="glitch" data-text="Rahul.">
+                Rahul.
+              </div>
+              <p>
+                I'll be a freshman at{" "}
+                <strong>Georgia Institute of Technology</strong> this fall,
+                majoring in Computer Science.
+              </p>
+              <p>
+                I’m fascinated by mathematics, computer science, robotics, and
+                the connections between the three.
+              </p>
+              <p>
+                When I’m not using a computer, I like to write poetry, learn
+                foreign languages, shoot photos on film, and play the Indian
+                classical flute–the bansuri. At a party, you’ll find me talking
+                to anyone who’ll lend an ear. Don’t hesitate to get in touch :)
+              </p>
+              <strong>
+                <em>scroll &raquo;</em>
+              </strong>
+              <footer>
+                <a href="mailto:rahulsachindeshpande@gmail.com"> email </a>
+                <a href="https://instagram.com/itsrahuldeshpande"> insta </a>
+                <a href="https://github.com/dishpanda"> github </a>
+              </footer>
             </div>
-          </div>
-
-          <div className="window-body">
-            <h2>Hi there! I'm Rahul.</h2>
-            <p>
-              I'll be a freshman at Cal Poly SLO this fall, majoring in Computer
-              Science.
-            </p>
-            <p>
-              I’m fascinated by mathematics, computer science, robotics, and the
-              connections between the three.
-            </p>
-            <p>
-              When I’m not using a computer, I like to write poetry, learn
-              foreign languages, shoot photos on film, and play the Indian
-              classical flute–the bansuri. At a party, you’ll find me talking to
-              anyone who’ll lend an ear. Don’t hesitate to get in touch :)
-            </p>
-            <div className="field-row footer" style={{ justifyContent: "center" }}>
-              <a href="https://github.com/dishpanda">
-                <button>GitHub</button>
-              </a>
-              <a href="https://www.instagram.com/itsrahuldeshpande/">
-                <button>Instagram</button>
-              </a>
-              <a href="mailto:rahulsachindeshpande@gmail.com">
-                <button>Email</button>
-              </a>
-            </div>
+          </Container>
+        </div>
+        <div className="right">
+          <div class="image">
+            <img class="object-fit_cover" alt="rahul" src="rahul.jpeg" />
           </div>
         </div>
       </div>
