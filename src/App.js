b99d47df49a9cab76304b53133b71f7d87b080cf
Rahul Deshpande <rahuldeshpande@gatech.edu>
2022-10-10 10:39:11 +0530

    redesigns website

--- src/App.js
+++ src/App.js
@@ -1,125 +1,107 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from "react-bootstrap";
-
-import { GrInstagram, GrGithub, GrDocument } from "react-icons/gr";
-import { GoMailRead } from "react-icons/go";
-import Resume from "./Deshpande_Rahul_BS_2023.pdf";
+import { setToLS, getFromLS } from "./utils/storage";
 
 function App() {
-  return (
-    <div className="page">
-      <div className="flex-container">
-        <div className="one">
-          <Container>
-            <div className="one-body">
-              <h3 className="text-uppercase">Hello, I'm</h3>
-              <div className="glitch" data-text="Rahul.">
-                Rahul.
-              </div>
-              <p>
-                I am a second year student at the{" "}
-                <strong>
-                  <a style={{ color: "white" }} href="https://www.gatech.edu/">
-                    Georgia Institute of Technology
-                  </a>
-                </strong>
-                , majoring in Computer Science 
-                {/* with a minor in public policy */}.
-              </p>
-              <p>
-                I’m fascinated by mathematics, computer science, political science, and
-                the connections between the three.
-              </p>
-              <p>
-                When I’m not using a computer, I like to write poetry, learn
-                foreign languages, shoot photos on film, and play the Indian
-                classical flute (the bansuri). At a party, you’ll find me talking
-                to anyone who’ll lend an ear. Don’t hesitate to get in touch :)
-              </p>
-              <p>
-                Some of my involvements:
-              </p>
-              <ul>
-                <li>The Technique Newspaper (Technology Editor)</li>
-                <li>College Democrats (VP, Logistics and Finance)</li>
-                <li>North Ave South Hall Council Officer (Communications Coordinator)</li>
-                <li>Astronomy Club (Webmaster)</li>
-                {/* <li>Student Government (IT Board member)</li>
-                <li>WREK Radio (Operator)</li> */}
-              </ul>
+  const [color, setColor] = useState("");
+  const [firstRender, setFirstRender] = useState(true);
 
-              <strong>
-                <em>scroll &raquo;</em>
-              </strong>
-              <footer>
-                <a href="mailto:rahuldeshpande@gatech.edu">
-                  <GoMailRead />
-                </a>{" "}
-                <a href="https://instagram.com/itsrahuldeshpande">
-                  <GrInstagram />
-                </a>{" "}
-                <a href="https://github.com/dishpanda">
-                  <GrGithub />
-                </a>{" "}
-                <a
-                  style={{ color: "blue" }}
-                  href={Resume}
-                  without
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  <GrDocument />
-                </a>{" "}
-              </footer>
-            </div>
-          </Container>
+  useEffect(() => {
+    const themes = ["green", "white", "blue", "yellow", "pink"];
+    const idx = Math.floor(Math.random() * themes.length);
+    const storedColor = getFromLS("color");
+    let newColor = themes[idx];
+
+    if (firstRender) {
+      if (storedColor) setColor(storedColor);
+    } else {
+      newColor =
+        themes[idx] === storedColor || themes[idx] === color
+          ? themes[Math.floor(Math.random() * themes.length)]
+          : newColor;
+      setColor(newColor);
+      setToLS("color", newColor);
+    }
+
+    setFirstRender(false);
+  }, [firstRender]);
+
+  return (
+    <div className={"wrapper " + color}>
+      <div className="content">
+        <div>
+          <h1>
+            <span>Rahul</span>
+            <span>Deshpande</span>
+            <span>CS@GT</span>
+          </h1>
         </div>
-        <div className="two">
-          <div className="image">
-            <img className="object-fit_cover" alt="rahul" src="rahul.png" />
-          </div>
+
+        <div className="info">
+          <p>Hey! I'm Rahul.</p>
+          <p>
+            I am a graduating third year undergraduate student at the{" "}
+            <strong>
+              <a href="https://www.gatech.edu/">
+                Georgia Institute of Technology
+              </a>
+            </strong>
+            , majoring in Computer Science.
+          </p>
+          <p>
+            I'm fascinated by computer science, journalism, data analytics, and
+            what lies at their intersection. I'm passionate about digital
+            civics, the environment, and the future of the internet.
+          </p>
+          <p>
+            When I'm not using a computer, I like to write poetry, learn foreign
+            languages, shoot photos on film, and play the Indian classical flute
+            (the bansuri). Don't hesitate to get in touch :)
+          </p>
+          <p>Some of my involvements:</p>
+          <ul id="nav">
+            <li>
+              <a href="https://nique.net/author/rahul/">
+                Technique Newspaper — Technology Editor
+              </a>
+            </li>
+            <li>
+              <a href="https://gatech.campuslabs.com/engage/organization/college-democrats">
+                College Democrats — President
+              </a>
+            </li>
+            <li>
+              <a href="https://https://www.astronomyclub.gatech.edu/">
+                Astronomy Club — Webmaster
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/gtwrecktechs/">
+                OIT Wreck Tech
+              </a>
+            </li>
+            <li>
+              <a href="https://www.cc.gatech.edu/mentoring-program#:~:text=Peer%20mentors%20are%20paired%20together,week%20during%20the%20fall%20semester.">
+                CoC CS 1100 Peer Mentor
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/aajaatlanta">
+                AAJA ATL — Student Coordinator
+              </a>
+            </li>
+          </ul>
+          {/* <p>
+            See my work <a href="portfolio.html">here.</a>
+          </p> */}
         </div>
-        {/* <div className="three">
-          <div className="three-bg"></div>
-          <Container className="">
-            <h1 className="mt-5 display-1">Work</h1>
-            <ol>
-              <li>
-                <h4>Cal High Robotics</h4>
-                <small className="text text-uppercase">2020-present</small>
-                <ul>
-                  <li>
-                    Team Captain, <em>2016-2020</em>
-                  </li>
-                  <li>
-                    Vice President <em>2018-2019</em>
-                  </li>
-                  <li>
-                    President <em>2019-2020</em>
-                  </li>
-                  <li>
-                    Head Mentor <em>2020-Present</em>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <h4>Tutor/TA, Ace Academy</h4>{" "}
-                <small className="text text-uppercase">summer 2019</small>
-              </li>
-              <li>
-                <h4>Dev Intern, Fiveable</h4>
-                <small className="text text-uppercase">summer 2019</small>
-              </li>
-            </ol>
-          </Container>
+        <hr />
+        <div className="footer">
+          &copy; Rahul Deshpande ad infinitum •{" "}
+          <a href="https://twitter.com/rsdesh47/">twitter</a>
+          {" • "}
+          <a href="https://www.linkedin.com/in/rsdeshpa/">linkedin</a>
         </div>
-        <div className="four">
-          <Container>
-            <Portfolio/>
-          </Container>
-        </div> */}
       </div>
     </div>
   );
